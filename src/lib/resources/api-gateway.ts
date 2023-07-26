52088b3f7ad3f420d756cb66057feae5883dedc8
Poojan Bhatt <pdbhatt010@gmail.com>
2023-07-26 18:31:18 +0530

    refactor: :recycle: API Gateway Response change
    Adding headers in API gateway response

--- src/lib/resources/api-gateway.ts
+++ src/lib/resources/api-gateway.ts
@@ -19,6 +19,15 @@ export const response = (
 ) => {
   return {
     statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        // eslint-disable-next-line max-len
+        "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,PATCH,DELETE,PUT,GET",
+    },
+    isBase64Encoded: false,
     body: JSON.stringify(response),
   };
 };
