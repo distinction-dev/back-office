e2fc4c292e0a40b9d343f7103c4692237e15376e
Sid2601 <siddharthshah2601@gmail.com>
2023-09-17 14:25:52 +0530

    feat: :sparkles: Global Seconadry index added in the dynamodb resrouce and also dynamodbstreams updated

--- src/resources/dynamoResources.ts
+++ src/resources/dynamoResources.ts
@@ -2,24 +2,42 @@ import { AWSDynamoDBTable } from "serverless-schema";
 import { DynamoDBTableNames } from "./constants";
 
 export const dynamoResources: AWSDynamoDBTable = {
-    Type: "AWS::DynamoDB::Table",
-    Properties: {
-      TableName: DynamoDBTableNames.BackOfficeTimeSheetTableName,
-      AttributeDefinitions: [
-        {
-          AttributeName: "id",
-          AttributeType: "S",
-        },
-      ],
-      KeySchema: [
-        {
-          AttributeName: "id",
-          KeyType: "HASH",
-        },
-      ],
-      BillingMode: "PAY_PER_REQUEST",
-      StreamSpecification: {
-        StreamViewType: "NEW_AND_OLD_IMAGES",
+  Type: "AWS::DynamoDB::Table",
+  Properties: {
+    TableName: DynamoDBTableNames.BackOfficeTimeSheetTableName,
+    AttributeDefinitions: [
+      {
+        AttributeName: "id",
+        AttributeType: "S",
       },
+    ],
+    KeySchema: [
+      {
+        AttributeName: "id",
+        KeyType: "HASH",
+      },
+    ],
+    BillingMode: "PAY_PER_REQUEST",
+    StreamSpecification: {
+      StreamViewType: "NEW_AND_OLD_IMAGES",
     },
+    GlobalSecondaryIndexes: [
+      {
+        IndexName: "DateNameIndex",
+        KeySchema: [
+          {
+            AttributeName: "date",
+            KeyType: "HASH",
+          },
+          {
+            AttributeName: "name",
+            KeyType: "RANGE",
+          },
+        ],
+        Projection: {
+          ProjectionType: "ALL",
+        },
+      },
+    ],
+  },
 };
