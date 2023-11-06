28b9b5777d45e94445414968dd0d5beef388ade9
Poojan Bhatt <pdbhatt010@gmail.com>
2023-11-06 16:55:19 +0530

    refactor: :recycle: refactored dynamo db streams lambda function

--- src/handlers/dynamodbStream.ts
+++ src/handlers/dynamodbStream.ts
@@ -1,109 +1,32 @@
-/* eslint-disable max-len */
-import {
-  putSingleItemDynamoDB,
-  queryDynamoDBTable,
-  updateItemDynamoDB,
-} from "@lib/resources/dynamo";
-import { DynamoDBTableNames } from "../resources/constants";
-import { unmarshall } from "@aws-sdk/util-dynamodb";
-import { DynamoDBStreamEvent } from "aws-lambda";
-import { sendBatchSQSMessage } from "@lib/resources/sqs";
 import { v4 as uuid } from "uuid";
+import { DynamoDBStreamEvent } from "aws-lambda";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { sendBatchSQSMessage } from "@lib/resources/sqs";
 
 import {
-  sendTimeSheetRecordToKimai,
   getDevsKimaiData,
+  sendTimeSheetRecordToKimai,
+  getKimaiRecordStartEndTimeStrings,
 } from "src/utils/kimaiUtils";
-import { QueryCommandInput } from "@aws-sdk/lib-dynamodb";
-
-const getStartEndTimeStrings = (
-  date: string,
-  start_hours: number,
-  end_hours: number
-) => {
-  const startTime = new Date(date);
-  startTime.setHours(start_hours);
-  const endTime = new Date(date);
-  endTime.setHours(end_hours);
-  return {
-    begin: startTime.toISOString().slice(0, -5),
-    end: endTime.toISOString().slice(0, -5),
-  };
-};
 
 export const handler = async (event: DynamoDBStreamEvent) => {
   const failedKimaiEntries = [];
   try {
     console.log({ event });
     for (const record of event.Records) {
-      console.log({ record });
-      console.log({ data: unmarshall(record.dynamodb.NewImage as any) });
-      if (record.eventName === "MODIFY") {
-        // Extract the updated item from the record
-        const updatedItem = unmarshall(record.dynamodb.NewImage as any);
+      console.log("Record", { record });
+      console.log("Record.dynamodb.NewImage", {
+        data: unmarshall(record.dynamodb.NewImage as any),
+      });
 
-        // Check if the date exists in the DynamoDB table for the editor
-        const params: QueryCommandInput = {
-          TableName: DynamoDBTableNames.TimeSheetDynamoTable,
-          KeyConditionExpression: "id = :key",
-          ExpressionAttributeValues: {
-            ":key": updatedItem?.id,
-          },
-        };
-        const existingItem = await queryDynamoDBTable(params);
-
-        if (!existingItem.Items.length) {
-          // If the date does not exist, create a new record
-          try {
-            await putSingleItemDynamoDB({
-              TableName: DynamoDBTableNames.TimeSheetDynamoTable,
-              Item: updatedItem,
-            });
-
-            console.log(
-              `Created new record for name: 
-            ${updatedItem.name} and Date: ${updatedItem.date}`
-            );
-          } catch (error) {
-            console.log("Dynamo Error:", error);
-          }
-        } else if (
-          existingItem.Items[0].lastEditedTime !== updatedItem.lastEditedTime
-        ) {
-          // If the date exists but lastEditedTime is different, update the record
-          try {
-            await updateItemDynamoDB({
-              TableName: DynamoDBTableNames.TimeSheetDynamoTable,
-              Key: {
-                name: updatedItem.name,
-                date: updatedItem.date,
-              },
-              UpdateExpression:
-                "SET lastEditedTime = :time, description = :description",
-              ExpressionAttributeValues: {
-                ":time": updatedItem.lastEditedTime,
-                ":description": updatedItem.description,
-              },
-            });
-            console.log(
-              `Updated record for name: ${updatedItem.name} and Date: ${updatedItem.date}`
-            );
-          } catch (error) {
-            console.log("Dynamo Error:", error);
-          }
-        } else {
-          console.log(
-            `No action needed for name: ${updatedItem.name} and Date: ${updatedItem.date}`
-          );
-        }
-      } else if (record.eventName === "INSERT") {
+      if (record.eventName === "INSERT") {
         // Extract the updated item from the record
         const createdItem = unmarshall(record.dynamodb.NewImage as any);
 
-        console.log({ createdItem });
+        console.log("Created Item", { createdItem });
 
         if (createdItem.customer === "Onmo Consulting") {
-          const { begin, end } = getStartEndTimeStrings(
+          const { begin, end } = getKimaiRecordStartEndTimeStrings(
             createdItem.date,
             10,
             18
@@ -113,13 +36,15 @@ export const handler = async (event: DynamoDBStreamEvent) => {
             createdItem.name?.trim(),
             process.env.NOTION_DB_KIMAI_TOKENS
           );
-          console.log({ devsKimaiData });
+
+          console.log("Kimai Data of developer", { devsKimaiData });
+
           const kimaiBody = JSON.stringify({
             begin,
             end,
+            description: "",
             project: devsKimaiData.projectId?.toString()?.trim(),
             activity: devsKimaiData.activityId?.toString()?.trim(),
-            description: "",
           });
           console.log("Kimai Record Body", kimaiBody);
 
@@ -141,21 +66,6 @@ export const handler = async (event: DynamoDBStreamEvent) => {
             }
           }
         }
-
-        await putSingleItemDynamoDB({
-          TableName: DynamoDBTableNames.TimeSheetDynamoTable,
-          Item: createdItem,
-        });
-        console.log(
-          `Created new record for name: ${createdItem.name} and Date: ${createdItem.date}`
-        );
-
-        // Check if the date exists in the DynamoDB table for the editor
-        const params = {
-          TableName: DynamoDBTableNames.TimeSheetDynamoTable,
-          Key: { name: createdItem.name, date: createdItem.date },
-        };
-        console.log(params);
       }
     }
   } catch (error) {
